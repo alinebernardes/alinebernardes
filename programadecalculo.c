d970fc89744fe759772ec1eb56f29043ab8899ea
alinebernardes <113390893+alinebernardes@users.noreply.github.com>
2022-09-19 18:38:45 -0300

    Add files via upload

---
+++ programadecalculo.c
@@ -0,0 +1,26 @@
+# include < studio.h>
+int main(){
+	 float numero1,numero2;
+	 float soma,subtração,multiplicação,divisão;
+	 
+	 printf(* calculadora top *\);
+	 printf(''Digite o primeiro numero'');
+	 scanf(''%f'',&numero1);
+	 
+	 printf(''Digite o segundo numero'');
+	 scanf(''f'',&numero2);
+	 
+	 soma=numero1+numero1;
+	 subtracao=numero1-numero2;
+	 multiplicacao=numero1*numero2;
+	 divisão=numero1/numero2;
+	 
+	 printf(''Numero 1: %.2f\n'',numero1);
+	 printf(''Numero 2: %.2f\n'',numero2);
+	 printf(''Soma: %f\n'',soma);
+	 printf(''Subtracao entre numero1 e numero2: %.2f\n'',subtracao);
+     printf(''multiplicacao: %.2f\n'',multiplicacao);
+     printf(''Divisao entre numero1 e numero2: %.2f\n'',divisão);
+
+     return 0;	 
+	 
